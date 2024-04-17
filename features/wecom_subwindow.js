90551c36062fa4ef14bb09e0543da76793be57ff
Tommy Cheng <xycheng@bu.edu>
2024-04-17 18:49:54 -0400

    Improve WeCom Connection functionality

---
+++ features/wecom_subwindow.js
@@ -0,0 +1,7 @@
+function overlay_on() {
+    document.getElementById("overlay").style.display = "block";
+}
+  
+function overlay_off() {
+    document.getElementById("overlay").style.display = "none";
+}
